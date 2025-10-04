33033b29ebf7b20903ae6d870c00e1f99027b868
Huynh_Ba_Anh <huynhbaanh03112003@gmail.com>
2025-10-04 16:39:04 +0700

    speak word

---
+++ src/component/speakWord.ts
@@ -0,0 +1,15 @@
+export function speakWord(word: string, lang: string) {
+  if ("speechSynthesis" in window) {
+    const utterance = new SpeechSynthesisUtterance(word);
+
+    // Chọn ngôn ngữ
+    if (lang === "en") utterance.lang = "en-US";
+    else if (lang === "vi") utterance.lang = "vi-VN";
+    else if (lang === "ja") utterance.lang = "ja-JP";
+
+    utterance.rate = 1; // tốc độ đọc
+    window.speechSynthesis.speak(utterance);
+  } else {
+    alert("Trình duyệt không hỗ trợ Speech Synthesis");
+  }
+}
